19b6b1440b11044b7d01ca16c3526be147046945
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2016-12-31 23:22:04 +0200

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var Navbar = require('Navbar');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Navbar/>
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
+module.exports = Main;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+console.log(require('react-router'));
+
+var Navbar = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <div className='top-bar'>
+            <div className='top-bar-left'>
+              <ul className="menu">
+                <li className='menu-text'>
+                  React Timer App
+                </li>
+                <li>
+                  <IndexLink to='/' activeClassName='active-link'>Timer</IndexLink>
+                </li>
+                <li>
+                  <Link to='/' activeClassName='active-link'>Countdown</Link>
+                </li>
+              </ul>
+            </div>
+            <div className='top-bar-right'>
+              <lu className='menu'>
+                <li className='menu-text'>
+                  Created by <a href="">EvGeniy</a> 
+                </li>
+              </lu>
+            </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Navbar;
